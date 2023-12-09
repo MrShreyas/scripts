82c4aa8b9a0a1eae7839480719e0a0068b39e0d6
Shreyas Katalkar <135653218+MrShreyas@users.noreply.github.com>
2023-12-10 03:27:21 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,1 +1,11 @@
 sudo apt-get install git-core subversion wget libjansson-dev sqlite autoconf automake libxml2-dev libncurses5-dev libtool
+sudo contrib/scripts/get_mp3_source.sh
+sudo contrib/scripts/install_prereq install
+sudo ./configure
+sudo make menuselect
+sudo make -j2
+sudo make install
+sudo make samples
+sudo make basic-pbx
+sudo make config
+sudo ldconfig
